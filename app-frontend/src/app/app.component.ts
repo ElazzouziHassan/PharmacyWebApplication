d7381af21d57d4d7558602048a3b8f16678d2e60
Tahiri Said <said.tahiiri@gmail.com>
2024-01-08 19:42:55 +0100

    Revert "added the upper NavBar"
    This reverts commit 848a46c484a4d60bbdced82efa982ae72648f72d.

--- app-frontend/src/app/app.component.ts
+++ app-frontend/src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
-import { NavbarComponent } from "./component/navbar/navbar.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, HomeComponent, HomeComponent, NavbarComponent]
+    imports: [CommonModule, RouterOutlet, HomeComponent,HomeComponent]
 })
 export class AppComponent {
   title = 'app-frontend';
