867d74accbb47a3f559e7c08af00ba67d1c022cb
Tahiri Said <said.tahiiri@gmail.com>
2024-01-08 19:51:43 +0100

    added the upper-navbar

--- app-frontend/src/app/app.component.ts
+++ app-frontend/src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
+import { NavbarComponent } from "./component/navbar/navbar.component";
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, HomeComponent,HomeComponent]
+    imports: [CommonModule, RouterOutlet, HomeComponent, HomeComponent, NavbarComponent]
 })
 export class AppComponent {
   title = 'app-frontend';
