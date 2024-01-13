db94ac7ca35809006b1e8fac1dc22281754f7ef3
= <=>
2024-01-13 20:07:38 +0100

    Ajouter route login

--- app-frontend/src/app/app.routes.ts
+++ app-frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ContactusComponent } from './pages/contactus/contactus.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 
 export const routes: Routes = [
-    {path:'contact-us',component:ContactusComponent}
+    {path:'contact-us',component:ContactusComponent},
+    {path:"login", component:LoginComponent}
 ];
