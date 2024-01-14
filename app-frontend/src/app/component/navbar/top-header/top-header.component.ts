15678998df757edac3ffd4dbf46400652be4e603
Tahiri Said <said.tahiri@hotmail.com>
2024-01-14 13:41:01 +0100

    added routerLink

--- app-frontend/src/app/component/navbar/top-header/top-header.component.ts
+++ app-frontend/src/app/component/navbar/top-header/top-header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'top-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './top-header.component.html',
   styleUrl: './top-header.component.scss'
 })
