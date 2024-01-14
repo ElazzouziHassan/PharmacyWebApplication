0be9d80041a78bb3b7e9a208d44377f975d017d6
Tahiri Said <said.tahiiri@gmail.com>
2024-01-14 01:47:03 +0100

    changes

--- app-frontend/src/app/app.routes.ts
+++ app-frontend/src/app/app.routes.ts
@@ -1,24 +1,32 @@
 import { Routes } from '@angular/router';
 import { ContactusComponent } from './pages/contactus/contactus.component';
-<<<<<<< HEAD
 import { LoginComponent } from './pages/login/login.component';
-=======
 import { HomeComponent } from './pages/home/home.component';
 import { ShopComponent } from './pages/shop/shop.component';
 import { BlogComponent } from './pages/blog/blog.component';
->>>>>>> cd1d9feb74fdcde76e65549d73bcca7893ee071c
+import { ShopDetailsComponent } from './pages/shop-details/shop-details.component';
+import { ShopingCartComponent } from './pages/shoping-cart/shoping-cart.component';
+import { CheckOutComponent } from './pages/check-out/check-out.component';
+import { BlogdetailsComponent } from './pages/blogdetails/blogdetails.component';
 
 
 
 export const routes: Routes = [
-<<<<<<< HEAD
+
     {path:'contact-us',component:ContactusComponent},
-    {path:"login", component:LoginComponent}
-=======
+    {path:"login", component:LoginComponent},
     {path:'',component:HomeComponent},
     {path:'contact-us',component:ContactusComponent},
     {path:'home',component:HomeComponent},
     {path:'shop',component:ShopComponent},
     {path:'blog',component:BlogComponent},
->>>>>>> cd1d9feb74fdcde76e65549d73bcca7893ee071c
+    {path:'shopdetails',component:ShopDetailsComponent},
+    {path:'shopingcart',component:ShopingCartComponent},
+    {path:'checkout',component:CheckOutComponent},
+    {path:'blogdetails',component:BlogdetailsComponent},
+    {path:'login',component:LoginComponent},
+
 ];
+
+
+                               
--- app-frontend/src/app/pages/login/login.component.ts
+++ app-frontend/src/app/pages/login/login.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+/* import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2'; */
+/* import { BrowserModule } from '@angular/platform-browser'; */
+import { NgModule } from '@angular/core';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  constructor(){
+
+  }
+  addedToCart(){
+  
+     Swal.fire({
+      title: "Product Added",
+      text: " Added To the card",
+      icon: "success",
+      timer:1500,
+      timerProgressBar:true
+    }); 
+  }
+  
+
 
 }
