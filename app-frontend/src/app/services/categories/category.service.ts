0d001207dcdf686e2a9e75d1118414b1a997499a
Tahiri Said <said.tahiri@hotmail.com>
2024-01-17 10:59:47 +0100

    added some HttpRequests for category Service

--- app-frontend/src/app/services/categories/category.service.ts
+++ app-frontend/src/app/services/categories/category.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,22 @@ import { Injectable } from '@angular/core';
 })
 export class CategoryService {
 
-  constructor() { }
+  SERVER_URL="http://localhost:3000/api";
+
+  constructor(private http:HttpClient) { 
+
+  }
+
+
+  getAllProducts(){
+    return this.http.get(this.SERVER_URL+"");
+
+  }
+  getOneProduct(id:Number){
+    return this.http.get(this.SERVER_URL+"");
+  }
+
+  getProductsByCategory(category:String){
+    return this.http.get(this.SERVER_URL+"");
+  }
 }
