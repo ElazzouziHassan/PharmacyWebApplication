9f27cad207730d54babf2a64c7479630d75ba891
Tahiri Said <said.tahiri@hotmail.com>
2024-01-17 11:05:33 +0100

    added some http request methods to users service

--- app-frontend/src/app/services/users/user.service.ts
+++ app-frontend/src/app/services/users/user.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,21 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
 
-  constructor() { }
+  
+  SERVER_URL="http://localhost:3000/api";
+
+  constructor(private http:HttpClient) { 
+
+  }
+
+  getAllUsers(){
+    return this.http.get(this.SERVER_URL+"");
+
+  }
+  getOneUser(id:Number){
+    return this.http.get(this.SERVER_URL+"");
+  }
+
+
+  
 }
