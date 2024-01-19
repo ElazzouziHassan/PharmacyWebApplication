ea71104e39661337362c2586cb0f2148639fee91
liberation <62626976+mohammedlatrach@users.noreply.github.com>
2024-01-19 23:35:23 +0100

    'owl-item' component is separated and it works

--- app-frontend/src/app/pages/home/home.component.ts
+++ app-frontend/src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { OwlItemComponent } from './owl/owl-item/owl-item.component';
 
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive,MainComponent],
+  imports: [RouterLink,RouterLinkActive,MainComponent,OwlItemComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ app-frontend/src/app/pages/home/owl/owl.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-owl',
+  standalone: true,
+  imports: [],
+  templateUrl: './owl.component.html',
+  styleUrl: './owl.component.scss'
+})
+export class OwlComponent {
+
+}
