185b68f028d3f14f78c6bc2c39183b0b21436269
liberation <62626976+mohammedlatrach@users.noreply.github.com>
2024-01-19 23:49:32 +0100

    separated 'owl-stage' into a separate component

--- app-frontend/src/app/pages/home/home.component.ts
+++ app-frontend/src/app/pages/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { OwlItemComponent } from './owl/owl-item/owl-item.component';
+import { OwlStageComponent } from './owl/owl-stage/owl-stage.component';
 
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive,MainComponent,OwlItemComponent],
+  imports: [RouterLink,RouterLinkActive,MainComponent,OwlItemComponent,OwlStageComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
