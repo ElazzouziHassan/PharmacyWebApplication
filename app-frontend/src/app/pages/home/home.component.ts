ccdd5881bafec732555bf0ef18492011c37839e1
liberation <62626976+mohammedlatrach@users.noreply.github.com>
2024-01-19 23:04:58 +0100

    created a 'main' component for the 'home' component, and put some 'home' component code into it

--- app-frontend/src/app/pages/home/home.component.ts
+++ app-frontend/src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink,RouterLinkActive,MainComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
