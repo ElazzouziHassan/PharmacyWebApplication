5e594b187c11ed02008396cec04a04e0893713dc
Tahiri Said <said.tahiri@hotmail.com>
2024-01-20 17:39:26 +0100

    Tahiri said : retrieved products from Product Service

--- app-frontend/src/app/pages/home/home.component.ts
+++ app-frontend/src/app/pages/home/home.component.ts
@@ -2,15 +2,27 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { OwlComponent } from './owl/owl.component';
+import { ProductService } from '../../services/products/product.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CurrencyPipe, NgFor } from '@angular/common';
 
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive,MainComponent,OwlComponent],
+  imports: [NgFor,RouterLink,RouterLinkActive,MainComponent,OwlComponent,HttpClientModule,CurrencyPipe],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  providers:[ProductService]
 })
 export class HomeComponent {
+
+  products:any;
+  
+  constructor(pros:ProductService){
+
+    this.products =  pros.getAllProducts();
+    console.log(this.products)
+  }
 
 }
