4224d96dfea6db0e01e45da939916879bd855ce8
Tahiri Said <said.tahiri@hotmail.com>
2024-01-20 16:15:04 +0100

    Tahiri Said : define products table and retrieve data from the Products Service

--- app-frontend/src/app/pages/shop/shop.component.ts
+++ app-frontend/src/app/pages/shop/shop.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, NgFor } from '@angular/common';
+import { Component,Pipe } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ProductService } from '../../services/products/product.service';
+import { HttpClientModule } from '@angular/common/http';
+import { pipe } from 'rxjs';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,NgFor,HttpClientModule,CurrencyPipe],
   templateUrl: './shop.component.html',
-  styleUrl: './shop.component.scss'
+  styleUrl: './shop.component.scss',
+  providers:[ProductService]
 })
 export class ShopComponent {
+
+  products:any;
+  
+  constructor(pros:ProductService){
+
+    this.products =  pros.getAllProducts();
+
+    console.log(this.products);
+  }
+  
+  
 
 }
